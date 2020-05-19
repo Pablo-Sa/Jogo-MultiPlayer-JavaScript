387c94a64e7882c4be785500cba1c4599b35d417
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2020-05-19 17:49:10 -0300

    Implementando Back-End NodeJS

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import http from 'http'
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static('./'))
+
+server.listen(3000, ()=> {
+    console.log(`> Server listening on port 3000`)
+
+
+}) 
